d0cca9fc8d544cea1296ac06cbf585f35b1f9655
Andy Vaughn <andy.vaughn@appfolio.com>
2023-11-10 15:44:22 -0800

    Add API endpoint for reindexing Algolia by triggering pipelines

--- backstage-plugin-algolia-backend/src/pipelines/index.ts
+++ backstage-plugin-algolia-backend/src/pipelines/index.ts
@@ -1,6 +1,7 @@
 export * from './types';
 export * from './BuilderBase';
 export * from './Pipeline';
+export * from './PipelineTrigger';
 export * from './Indexer';
 export * from './IndexManager';
 export * from './TechDocsCollatorFactory';
