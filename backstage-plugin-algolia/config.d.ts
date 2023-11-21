27b32c9ea47006e55b6bd2f24dba1b8a83ff0c6a
Andy Vaughn <andy.vaughn@appfolio.com>
2023-11-21 09:12:26 -0800

    Add frontend configuration

---
+++ backstage-plugin-algolia/config.d.ts
@@ -0,0 +1,30 @@
+export interface Config {
+  algolia: {
+
+    /**
+     * @visibility frontend
+     */
+    apikey: string;
+
+    /**
+     * @visibility frontend
+     */
+    applicationId: string;
+    indexes: {
+      techdocs: {
+
+        /**
+         * @visibility frontend
+         */
+        name: string;
+      };
+    };
+
+    /**
+     * Enable Algolia event tracking
+     * @visibility frontend
+     * @see https://www.algolia.com/doc/guides/building-search-ui/events/react/
+     */
+    insights?: boolean;
+  };
+}
