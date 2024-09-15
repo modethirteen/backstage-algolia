8166dc609ecf31795efc8a2f9972934621503dc8
Andy Vaughn <andy.vaughn@appfolio.com>
2024-09-15 13:06:34 -0700

    Simplify common index object type to be generic among all sources

--- backstage-plugin-algolia-common/src/types.ts
+++ backstage-plugin-algolia-common/src/types.ts
@@ -1,32 +1,10 @@
-import { CompoundEntityRef } from '@backstage/catalog-model';
-
 export interface IndexObject {
   source: string;
   title: string;
   text: string;
   location: string;
-  path: string;
-  section: boolean;
-  summary?: string;
-  topics?: string[];
-  entity?: {
-    kind: string;
-    namespace: string;
-    name: string;
-    title?: string;
-    type?: string;
-    lifecycle?: string;
-    owner?: string;
-    ownerRef?: CompoundEntityRef;
-    parent?: string;
-    parentRef?: CompoundEntityRef;
-    domain?: string;
-    domainRef?: CompoundEntityRef;
-    system?: string;
-    systemRef?: CompoundEntityRef;
-    memberOf?: string[];
-    memberOfRefs?: CompoundEntityRef[];
-  };
+  keywords: string[];
+  data?: object;
 }
 
 export interface IndexObjectWithIdAndTimestamp extends IndexObject {
